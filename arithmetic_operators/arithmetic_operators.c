4bb6649cdd0107f2164379b517191b677c748c50
Nixon Azeredo <39866741+NixonAzeredo@users.noreply.github.com>
2023-06-14 08:20:35 -0300

    arithmetic operators

---
+++ arithmetic_operators/arithmetic_operators.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// addintion operator
+// int main()
+// {
+//     int length = 10, width = 5, area;
+//     area = length + width;
+//     printf("%d \n", area);
+//     return 0;
+// }
+
+// division operator and modulus division
+
+// int main()
+// {
+//     int i1 = 10, i2 = 3, quotient, remainder;
+//     float f1 = 4.2, f2 = 2.5, result;
+
+//     quotient = i1 / i2;
+//     remainder = i1 % i2;
+//     result = f1 / f2;
+
+//     printf("Quotient: %d\nRemainder: %d\nResult: %3.2f", quotient, remainder, result);
+
+//     return 0;
+// }
+
+// Operators precedence
+// int main()
+// {
+//     int a = 6, b = 4, c = 2, result;
+//     result = a - b + c;
+//     printf("%d \n", result); // 4
+
+//     result = a + b / c;
+//     printf("%d \n", result); // 8
+
+//     result = (a + b) / c;
+//     printf("%d \n", result); // 5
+
+//     return 0;
+// }
+
+// Type Conversion
+
+int main()
+{
+    return 0;
+}
